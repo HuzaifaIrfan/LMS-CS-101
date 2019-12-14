0cd6ce23e38480cb91f3ca60fd62f9378576430a
Huzaifa Irfan <huzaifairfan2001@gmail.com>
2019-12-14 21:03:50 +0500

    user panel

--- userpanel.cpp
+++ userpanel.cpp
@@ -28,6 +28,8 @@ void saveuser(){
 
 
 void setupuser(){
+	    myvect.clear(); 
+		userbooksvect.clear(); 
 	for (auto &user: users) {
            vector<string> uservect;
            tokenize(user,'|', uservect);
@@ -40,7 +42,6 @@ void setupuser(){
 							
 				   for (auto &vectitem: uservect) {
 					   myvect.push_back(vectitem);
-					   cout<<vectitem<<endl;
 				   }
 			
 			}
@@ -57,6 +58,7 @@ void my_books(){
                     vector<string> userbooksvect;
                      tokenize(myvect[2],'&',  userbooksvect);
 
+
             for (auto &bookid: userbooksvect) {
 
 
@@ -70,7 +72,7 @@ void my_books(){
 
                                 //outstanding books
 
-    int duedays=5;
+    int duedays=1;
 
 
     time_t now = time(0);
@@ -82,18 +84,20 @@ void my_books(){
 
     int difference=timenow-timedue;
 
-    if(difference > (86400*duedays)){
-       
- int passed=difference/86400;
-        //  cout<<"Outstanding book"<<endl;
 
-        cout<<"Book Name :"<< bookvect[1]<<endl;
+
+
+	   		 cout<<"Book ID : "<< bookvect[0]<<endl;
+        cout<<"Book Name : "<< bookvect[1]<<endl;
+
+    if(difference > (86400*duedays)){
+ 	int passed=difference/86400;
+        //  cout<<"Outstanding book"<<endl;
         cout<<"Days Passed from due date : "<< passed<<endl<<endl;
     }
 	else{
 
- 	int remaining=((difference)/86400);
-		        cout<<"Book Name :"<< bookvect[1]<<endl;
+ 	int remaining=(timedue-timenow)/86400;
 				cout<<"Days Remaining till due date : "<< remaining<<endl<<endl;
 	}
 
@@ -111,19 +115,17 @@ void my_books(){
          cout<<"\tNo Books"<<endl;
     }
 
-
-         
-
-     
+}
 
 
+void showmybooks(){
+
+	my_books();
 
                 cout<<"\t0. EXIT\n";
                 char ch;
                 cout << "\t " ;
                 cin>>ch;
-
-
 }
 
 
@@ -131,6 +133,15 @@ void my_books(){
 void book_loan(){
 
    system("clear");
+
+if(userbooksvect.size()>=4){
+
+
+cout<<"You cannot loan more than 3 books\n";
+
+
+}else{
+
 	cout<< "\tAvailable Books"<<endl;
 
 	for (auto &abook: books) {
@@ -148,6 +159,9 @@ void book_loan(){
                 string inpbookid;
                 cout << "\t " ;
                 cin>>inpbookid;
+
+
+
 
 
 
@@ -177,7 +191,11 @@ void book_loan(){
 			    time_t now = time(0);
     			int timenow = now ;
 
-			string bookdue= inttostr(timenow);
+	int duedays=5;
+
+	int duetime=duedays*86400;
+
+			string bookdue= inttostr(timenow+duetime);
 
                 delete_line("books.db",i+2); 
 
@@ -230,6 +248,130 @@ void book_loan(){
 			cout<<"\t" << bookname <<" loaned to "<<username<<endl;
 	}
 
+	setup();
+	setupuser();
+
+
+
+
+}
+
+                cout<<"\t0. EXIT\n";
+                char ch;
+                cout << "\t " ;
+                cin>>ch;
+
+}
+
+
+void removebookid(string abookid){
+
+	string temp="0";
+
+
+
+  for (int i =1; i<userbooksvect.size(); i++) { 
+
+        string abook= userbooksvect[i];
+
+		if(abook!=abookid){
+			temp= temp+ "&"+abook;
+		}
+
+
+  }
+	userbooks=temp;
+}
+
+
+void book_deposit(){
+
+	my_books();
+
+
+                cout<<"\tEnter Book ID to Return\n";
+                string inpbookid;
+                cout << "\t " ;
+                cin>>inpbookid;
+
+
+
+	bool idmatch=false;
+
+  for (auto &abookid: userbooksvect) {
+	if(abookid==inpbookid){
+	idmatch=true;
+	}
+  }
+
+
+
+
+
+
+
+
+
+	cout<<"\n\n\n";
+
+
+	if(idmatch==false){
+	cout<<"\tInvalid Book ID\n";
+	}else{
+
+
+
+
+
+
+
+  for (int i =0; i<books.size(); i++) { 
+
+        string abook= books[i];
+           vector<string> bookvect;
+           tokenize(abook,'|', bookvect);
+
+
+            if(bookvect[0]==inpbookid){
+				idmatch=true;
+
+
+		   	bookid=bookvect[0];
+            bookname=bookvect[1];
+			bookauthor=bookvect[2];
+			string bookuser="0";
+			string bookdue=bookvect[4];
+
+
+			                delete_line("books.db",i+2); 
+
+				string bookstr="\n"+bookid+"|"+bookname+"|"+bookauthor+"|"+bookuser+"|"+bookdue;
+				  ofstream out("books.db", ios::app);
+  				out << bookstr;
+
+
+
+
+   for (int j =0; j<users.size(); j++) { 
+
+        string auser= users[j];
+           vector<string> uservect;
+           tokenize(auser,'|', uservect);
+
+			if(uservect[0]==userid){
+
+
+				delete_line("users.db",j+2);
+
+				removebookid(bookid);
+
+				saveuser();
+
+			}
+
+
+
+	}
 
 
 
@@ -240,10 +382,26 @@ void book_loan(){
 
 
 
+			}
+
+
+
+  }
 
 
 
 
+
+
+
+
+			cout<<"\t" << bookname <<" returned by "<<username<<endl;
+	}
+
+
+
+	setup();
+	setupuser();
 
 
                 cout<<"\t0. EXIT\n";
@@ -251,30 +409,41 @@ void book_loan(){
                 cout << "\t " ;
                 cin>>ch;
 
-				setup();
-				setupuser();
-
-}
-
-
-void book_deposit(){
-
-	
 }
 
 
 
 
-void user_login()
-{
+void user_login(){
 
-	username="huzaifa";
-	// cout<<"Enter Your name??\n";
-	// cin>>name;
+	username="ezaz";
+	cout<<"Enter Your name??\n";
+	cin>>username;
 
-	setup();
-	setupuser();
-   user_panel();
+	bool loginstate=false;
+
+   for (auto &user: users) {
+           vector<string> uservect;
+           tokenize(user,'|', uservect);
+
+	if(uservect[1]==username){
+
+	loginstate=true;
+
+	}
+	}
+
+	if(loginstate=true){
+
+		setup();
+		setupuser();
+		user_panel();
+	}else{
+		cout<<"Wrong Username"<<endl;
+	}
+
+
+
 }
 
 
@@ -287,6 +456,7 @@ void user_panel()
         system("clear");
       
 	char ch;
+	cout << "\n\t Logged in as "<<username << endl;
 		cout << "\n\t User Panel" << endl;
 		cout << "\n\t1. My Books" << endl;
 		cout << "\t2. Loan a book" << endl;
@@ -297,7 +467,8 @@ cin>>ch;
 		switch (ch)
 		{
 		case '1':
-			my_books();
+			showmybooks();
+			break;
 		case '2':
 			book_loan();
 			break;
